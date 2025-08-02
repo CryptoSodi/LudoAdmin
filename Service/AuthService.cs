15cdd84b9c70912b63fc92bf7a5e442fe199837d
Sodi <tassaduq009@gmail.com>
2025-08-02 12:27:26 +0500

    transactions system added

--- Service/AuthService.cs
+++ Service/AuthService.cs
@@ -30,7 +30,7 @@
 
         public async Task<PlayerInfo> GetPlayerInfoAsync()
         {
-            string json = await _js.InvokeAsync<string>("localStorage.getItem", Key);
+            var json = await _js.InvokeAsync<string>("localStorage.getItem", Key);
             if (string.IsNullOrWhiteSpace(json))
                 return null;
 
