7eff6e638875359206c1b5f5a2a62b0543638ff0
Sodi <tassaduq009@gmail.com>
2025-08-11 03:37:35 +0500

    fixed google login for xampp version added signalr for website link ie ludocities.com

--- Service/GlobalConstants.cs
+++ Service/GlobalConstants.cs
@@ -5,11 +5,11 @@ namespace LudoAdmin.Service
 {
     internal class GlobalConstants
     {
-        public static readonly bool Debug = true;
+        public static readonly bool Debug = false;
         static GlobalConstants()
         {
-            Url = Debug ? "https://localhost" : "https://13.202.76.246";
-            HubUrl = Url + ":8085/";
+            Url = Debug ? "https://localhost" : "https://www.ludocities.com";
+            HubUrl = Debug ? "https://localhost:8085/" : "https://www.ludocities.com/";
         }
         public static string HubUrl { get; internal set; }
         public static string Url { get; private set; }
